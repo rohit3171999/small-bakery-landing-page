dd8f54eb5e5446dccd4a3882a4c304a16f0bd5bb
rohit3171999 <rohit3171999@gmail.com>
2025-09-24 15:43:44 +0530

    add functional component in AboutBakery.jsx

---
+++ src/components/AboutBakery.jsx
@@ -0,0 +1,17 @@
+/*
+* Purpose: Share bakery’s origin, values, and focus on freshness & handmade products.
+
+* */
+export default function AboutBakery(){
+    return (
+        <>
+            {/*<section> (wrapper)*/}
+
+            {/*    <h2> (section title: “About Us”)*/}
+
+            {/*        <p> (short story about bakery, ingredients, values)*/}
+
+            {/*            <img> (optional: bakery interior or founder image)*/}
+        </>
+    );
+}
