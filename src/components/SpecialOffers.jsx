28a5def4f96a726af78754c1f92084835218711f
rohit3171999 <rohit3171999@gmail.com>
2025-09-24 16:10:16 +0530

    add functional component in SpecialOffers.jsx

---
+++ src/components/SpecialOffers.jsx
@@ -0,0 +1,24 @@
+/*
+* Purpose: Highlight bakery’s signature items and seasonal offers.
+* */
+export default function SpecialOffers(){
+    return (
+        <>
+{/*            <section> (wrapper)*/}
+
+{/*                <h2> (section title: “Special Offers”)*/}
+
+{/*                    <div> (cards for offers or bestsellers)*/}
+
+{/*                        <img> (item image)*/}
+
+{/*                            <h3> (item name)*/}
+
+{/*                                <p> (description or offer details)*/}
+
+{/*                                    <span> (discount/price)*/}
+
+{/*<button> (CTA: “Order Now”)*/}
+        </>
+    );
+}
